d312ac5593219be2d57d30ecacfa1fe84fd4e13c
Charles Rambo <76271967+charlesrambo@users.noreply.github.com>
2024-04-17 13:15:03 -0700

    Add files via upload

--- Chapter_3.py
+++ Chapter_3.py
@@ -34,9 +34,10 @@ def getDailyVol(close, span = 126, percent = False):
     
     return vol
 
+
 def applyPtS1OnT1(close, events, ptS1, molecule):
     """
-
+    
     Parameters
     ----------
     close : pandas series
@@ -77,7 +78,7 @@ def applyPtS1OnT1(close, events, ptS1, molecule):
         
     else:
         
-        pt = pd.Series(index = events.index)
+        pt = pd.Series(index = events_.index)
         
     if ptS1[1] > 0:
         
@@ -85,17 +86,17 @@ def applyPtS1OnT1(close, events, ptS1, molecule):
     
     else:
         
-        s1 = pd.Series(index = events.index)
+        s1 = pd.Series(index = events_.index)
         
     for t0, t1 in events_['t1'].fillna(close.index[-1]).items():
           
         # Get the price path
         path = (close[t0:t1]/close[t0] - 1) * events_.at[t0, 'side']
 
-        # Earliet profit taking      
+        # Earliest profit taking      
         out.loc[t0, 'pt'] = path[path > pt[t0]].index.min()
         
-        # Earliet stop loss
+        # Earliest stop loss
         out.loc[t0, 's1'] = path[path < s1[t0]].index.min() 
      
     return out
@@ -104,24 +105,26 @@ def applyPtS1OnT1(close, events, ptS1, molecule):
 def getEvents(close, tEvents, ptS1, trgt, minRet, numThreads, numDays = None, 
               verticalBarrier = False, side = None):
     
-    # 1) Get target
+    # Get target
     trgt = trgt.loc[tEvents]
-    trgt = trgt[trgt > minRet] # minRet
     
-    # 2) Get the max holding period t1
+    # Drop observations below minRet
+    trgt = trgt[trgt > minRet]
+    
+    # Get the max holding period t1
     if verticalBarrier:
         
         t1 = close.index.searchsorted(tEvents + pd.Timedelta(days = numDays))
         t1 = t1[t1 < close.shape[0]]
         
-        # NaNs at tend
+        # NaNs at end
         t1 = pd.Series(close.index[t1], index = tEvents[:t1.shape[0]])
         
     else:
         
         t1 = pd.Series(pd.NaT, index = tEvents)
     
-    # 3) Form events object, apply stop loss on t1
+    # Form events object, apply stop loss on t1
     if side is None:
         
         side_ = pd.Series(1.0, index = trgt.index)
@@ -135,7 +138,7 @@ def getEvents(close, tEvents, ptS1, trgt, minRet, numThreads, numDays = None,
     events = pd.concat({'t1':t1, 'target':trgt, 'side':side_}, 
                        axis = 1).dropna(subset = ['target'])
        
-    # 4) Use run_queued_multiprocessing to run applyPtS1OnT1 using multithreading
+    # Use run_queued_multiprocessing to run applyPtS1OnT1 using multithreading
     events = twenty.run_queued_multiprocessing(func = applyPtS1OnT1, 
                                          index = events.index, 
                                          num_threads = numThreads, 
@@ -143,10 +146,13 @@ def getEvents(close, tEvents, ptS1, trgt, minRet, numThreads, numDays = None,
                                          close = close, events = events,
                                          ptS1 = ptS1_)
     
+    # Drop missing values
     events['t1'] = events.dropna(how = 'all').min(axis = 1)
     
+    # If we didn't specify side...
     if side is None:
         
+        # ... drop column from events
         events = events.drop('side', axis = 1)
     
     return events
@@ -167,29 +173,37 @@ def getBins(events, close):
         Case 2: 'side' in events and bin in [0, 1]
     close : pandas series
         Closing price information. The index is the date of the closing price.
-        DESCRIPTION.
 
     Returns
     -------
     out : data frame
-        Labels for each observation of events where all necessary information is
-        defined.
+        Labels for each observation of events where all necessary information 
+        is defined.
 
     """
     
-    # 1) prices aligned with events
+    # Make it so prices aligned with events
     events_ = events.dropna(subset = ['t1'])
-    px = events_.index.union(events_['t1'].values).drop_duplicates()
-    px = close.reindex(px, method = 'bfill')
     
-    # 2) create out object
+    # Extend the events index
+    idx = events_.index.union(events_['t1'].values).drop_duplicates()
+    
+    # Back fill (?) missing prices
+    px = close.reindex(idx, method = 'bfill')
+    
+    # Create out object with index the same as events_
     out = pd.DataFrame(index = events_.index)
+    
+    # Calculate return
     out['ret'] = px.loc[events_['t1'].values].values/px.loc[events_.index] - 1
 
+    # If side is in events_...
     if 'side' in events_:
         
+        # ... adjust return accordingly
         out['ret'] *= events_['side']
-        
+    
+    # Define bin based on sign
     out['bin'] = np.sign(out['ret'])
     
     if 'side' in events_:
@@ -198,18 +212,25 @@ def getBins(events, close):
     
     return out
 
+
 def dropLabels(events, minPct = 0.05):
     
     # Apply weights, drop labels with insufficient examples
     while True:
         
-        df = events['bin'].value_couts(normalize = True)
+        # Get normalized count of bins
+        bin_df = events['bin'].value_counts(normalize = True)
         
-        if df.min() > minPct or df.shape[0] < 3:
+        # Break if all bin types > 5% or only two bin types left
+        if bin_df.min() > minPct or bin_df.shape[0] < 3:
             
             break
         
-        print(f'Dropped label {df.argmin()} which was {100 * df.min():.2f}% of observations.')
+        # Drop smallest bin type
+        events = events.loc[events['bin'] != bin_df.argmin(), :]
+        
+        # Print a message so we know what's going on
+        print(f'Dropped label {bin_df.argmin()}. It was {100 * bin_df.min():.2f}% of observations.')
         
     return events
 
@@ -245,70 +266,69 @@ def get_triple_barrier_label(x, upper = np.inf, lower = -np.inf, log_rtn = True,
         # ... return either 0 or the sign of the final return
         return 0 if zero_label else np.sign(path[-1])
     
-    # If only upper_args if empty...
+    # If only upper_args is empty...
     elif len(upper_args) == 0:
         
-        # ... then the lower_args isn't empty so return -1
+        # ... then lower_args isn't empty so return -1
         return -1
     
     # If only lower_args is empty...
     elif len(lower_args) == 0:
         
-        # ... then the upper_args isn't empy so return 1 
+        # ... then upper_args isn't empy so return 1 
         return 1
     
     # If neither list is empty... 
     else:
         
         # ... get first element of lists
-        upper_arg = upper_args[0]
-        lower_arg = lower_args[0]
+        upper_arg, lower_arg = upper_args[0], lower_args[0]
         
         # Assign label based on what happened first
         return 1 if upper_arg <= lower_arg else -1
     
-
-import matplotlib.pyplot as plt
-
-upper = 0.10
-lower = -0.07
-
-fig, ax = plt.subplots(nrows = 3, ncols = 2)
-
-plt.suptitle(r'Triple Barrier Label')
-
-
-for i in range(6):
-    
-    row, col = i//2, i % 2
-   
-    t = np.arange(1, 61, 1)
-    x = np.random.normal(0, 0.30/np.sqrt(250), size = 60)
-    p = 50 * np.exp(np.cumsum(x))
-    
-    label = get_triple_barrier_label(x, upper = upper, lower = lower, 
-                                     log_rtn = True, zero_label = True)
-    
-    ax[row, col].set_title(f'The label is {label}.')
-    
-    ax[row, col].hlines((1 + upper) * 50, xmin = np.min(t), xmax = np.max(t), 
-                        color = 'red')
-    ax[row, col].hlines((1 + lower) * 50, xmin = np.min(t), xmax = np.max(t), 
-                        color = 'red')
-    
-    ax[row, col].vlines(np.max(t), ymin = (1 + lower) * 50, 
-                        ymax = (1 + upper) * 50, 
-                        color = 'red')
-    
-    ax[row, col].plot(t, p)
-    
-    ax[row, col].hlines(50, xmin = np.min(t), xmax = np.max(t), color = 'gray', 
-               linestyles = 'dashed')
-
-plt.subplots_adjust(hspace = 1)
-
-plt.show()
-
+# =============================================================================
+# import matplotlib.pyplot as plt
+# 
+# upper, lower = 0.10, -0.07
+# 
+# fig, ax = plt.subplots(nrows = 4, ncols = 2)
+# 
+# plt.suptitle(r'Triple Barrier Label')
+# 
+# 
+# for i in range(8):
+#     
+#     row, col = i//2, i % 2
+#    
+#     t = np.arange(1, 61, 1)
+#     x = np.random.normal(0, 0.30/np.sqrt(250), size = 60)
+#     p = 50 * np.exp(np.cumsum(x))
+#     
+#     label = get_triple_barrier_label(x, upper = upper, lower = lower, 
+#                                      log_rtn = True, zero_label = True)
+#     
+#     ax[row, col].set_title(f'The label is {label}.')
+#     
+#     ax[row, col].hlines((1 + upper) * 50, xmin = np.min(t), xmax = np.max(t), 
+#                         color = 'red')
+#     ax[row, col].hlines((1 + lower) * 50, xmin = np.min(t), xmax = np.max(t), 
+#                         color = 'red')
+#     
+#     ax[row, col].vlines(np.max(t), ymin = (1 + lower) * 50, 
+#                         ymax = (1 + upper) * 50, 
+#                         color = 'red')
+#     
+#     ax[row, col].plot(t, p)
+#     
+#     ax[row, col].hlines(50, xmin = np.min(t), xmax = np.max(t), color = 'gray', 
+#                linestyles = 'dashed')
+# 
+# plt.subplots_adjust(hspace = 1)
+# 
+# plt.show()
+# 
+# =============================================================================
     
         
     
